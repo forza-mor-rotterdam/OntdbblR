2f51475ef492ad7dd44878ecec34be4d9d76a199
Jorrit Strikwerda <jippiejorrit@gmail.com>
2024-03-20 12:19:14 +0100

    Remove unneeded frontend. Update ci-cd to use KUBE_NAMESPACE. Deploy to test and acc on develop merge. Add deploy_date to health.

--- app/config/context_processors.py
+++ app/config/context_processors.py
@@ -25,4 +25,5 @@ def general_settings(context):
         "LOGOUT_URL": reverse("oidc_logout"),
         "LOGIN_URL": f"{reverse('oidc_authentication_init')}?next={absolute(context).get('FULL_URL')}",
         "GIT_SHA": settings.GIT_SHA,
+        "DEPLOY_DATE": settings.DEPLOY_DATE,
     }
--- app/frontend/assets/constants/environment.js
+++
@@ -1,9 +0,0 @@
-const API_ROOT = process.env.API_ROOT;
-const NODE_ENV = process.env.NODE_ENV;
-const GITHUB_SHA = process.env.GITHUB_SHA;
-
-export {
-	API_ROOT,
-	NODE_ENV,
-	GITHUB_SHA
-};
