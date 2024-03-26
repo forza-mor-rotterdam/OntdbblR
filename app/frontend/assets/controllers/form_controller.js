edfa944236647d7db3d2150235d355a631f34ab2
Jorrit Strikwerda <jippiejorrit@gmail.com>
2024-03-26 10:05:23 +0100

    Add prettier, djlint, stylelint to pre-commit. Linted and formatted.

--- app/frontend/assets/controllers/form_controller.js
+++ app/frontend/assets/controllers/form_controller.js
@@ -1,41 +1,40 @@
-import { Controller } from '@hotwired/stimulus';
+import { Controller } from '@hotwired/stimulus'
 
 let selectOptions = null
 
 export default class extends Controller {
+  static targets = ['searchSelect']
 
-    static targets = [ "searchSelect" ]
-
-    initialize() {
-        let self = this
-        selectOptions = []
-        if (self.hasSearchSelectTarget){
-            const options = self.searchSelectTarget.querySelectorAll("option")
-            for(let i = 0; i < options.length; i++){
-                selectOptions.push({value: options[i].value, label: options[i].textContent})
-            }
-        }
+  initialize() {
+    let self = this
+    selectOptions = []
+    if (self.hasSearchSelectTarget) {
+      const options = self.searchSelectTarget.querySelectorAll('option')
+      for (const option in options) {
+        selectOptions.push({ value: option.value, label: option.textContent })
+      }
     }
+  }
 
-    connect() {}
-    searchFieldChange(e){
-        let self = this
-        if (!self.hasSearchSelectTarget){
-            return
-        }
-        self.searchSelectTarget.innerHTML = ''
-        for(let i = 0; i < selectOptions.length; i++){
-            const re = new RegExp(e.target.value, "gi");
-            let option = document.createElement("option")
-            if (re.test(selectOptions[i].label)){
-                // let newContent = selectOptions[i].label
-                option.value = selectOptions[i].value
-                option.textContent = selectOptions[i].label
-                // option.innerHTML = newContent.replace(re, function(match) {
-                //     return "<mark style='color:red;'>" + match + "</mark>";
-                // })
-                self.searchSelectTarget.appendChild(option)
-            }
-        }
+  connect() {}
+  searchFieldChange(e) {
+    let self = this
+    if (!self.hasSearchSelectTarget) {
+      return
     }
+    self.searchSelectTarget.innerHTML = ''
+    const re = new RegExp(e.target.value, 'gi')
+    for (const selectOption in selectOptions) {
+      let option = document.createElement('option')
+      if (re.test(selectOption.label)) {
+        // let newContent = selectOptions[i].label
+        option.value = selectOption.value
+        option.textContent = selectOption.label
+        // option.innerHTML = newContent.replace(re, function(match) {
+        //     return "<mark style='color:red;'>" + match + "</mark>";
+        // })
+        self.searchSelectTarget.appendChild(option)
+      }
+    }
+  }
 }
